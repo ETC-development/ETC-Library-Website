4bc009bdb57b1ad11ee3edf2840dc7f3549a62da
Mohcen <hugoboy2311@gmail.com>
2022-08-15 15:18:21 +0200

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/NavBar/index";
 
 function App() {
   return (
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <>
+  <Navbar />
+  </>
  );
 }
 
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav>
+        
+    </nav>
+  )
+}
+
+export default NavBar
