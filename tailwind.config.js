d8947e50bd49fa82b6359622a5f4156c9f2294a0
Su-mia <106320367+Su-mia@users.noreply.github.com>
2022-08-17 00:48:26 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,css,js}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'dark-bg': '#0C1F47',
+      'light-bg': '#F4F4F4',
+      'light-text': '#F6F9F6',
+      'dark-text': '#0C1F47',
+      'secondary': '#F37D00',
+      'violet': '#9FADFD'
+    },
+    extend: {
+      fontFamily: {
+        'sans' : ['Montserrat', '"Open Sans"']
+      },
+      height: {
+        '125': "12.5%"
+      }
+    },
   },
   plugins: [],
 }
+
