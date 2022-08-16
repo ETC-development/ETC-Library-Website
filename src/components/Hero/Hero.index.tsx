670b6accb7c46e3bfa1a93c8d95dbdd7483e0593
salahmak <salah.mak@protonmail.com>
2022-08-16 19:38:04 +0100

    Code formatting

--- src/components/Hero/Hero.index.tsx
+++ src/components/Hero/Hero.index.tsx
@@ -1,12 +1,11 @@
-import HeroIllustration from "assets/hero.svg"
-import Typed from "react-typed"
-import './Hero.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import HeroIllustration from "assets/hero.svg";
+import Typed from "react-typed";
+import "./Hero.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAnglesUp} from "@fortawesome/free-solid-svg-icons";
 
 import quotes from "utils/quotes";
-import Wave from "../../utils/Wave";
-
+import Wave from "utils/Wave";
 
 const Hero: React.FunctionComponent = () => {
     return (
@@ -14,8 +13,7 @@ const Hero: React.FunctionComponent = () => {
             {/* Illustration + Paragraph */}
             <div
                 className="bg-dark-bg lg:h-fit w-full flex-grow-0 flex-shrink flex flex-no-wrap flex-col-reverse justify-center content-center px-2 lg:flex-row lg:flex-row lg:justify-around lg:px-10">
-                <div
-                    className="flex flex-shrink flex-col text-center lg:text-left gap-4 justify-center content-center">
+                <div className="flex flex-shrink flex-col text-center lg:text-left gap-4 justify-center content-center">
                     <h1 className="hero-h1">
                         <span>Welcome to </span>
                         <span className="text-secondary">ENSIA Bib</span>
@@ -41,18 +39,13 @@ const Hero: React.FunctionComponent = () => {
                         <span className="text-violet">everyone </span>
                         <span>with </span>
                         <span className="text-secondary block lg:inline">
-                            <Typed
-                                strings={[
-                                    "Exams.",
-                                    "Worksheets.",
-                                    "Tests.",
-                                    "Courses."
-                                ]}
-                                typeSpeed={150}
-                                backSpeed={50}
-                                loop
-                            />
-                        </span>
+              <Typed
+                  strings={["Exams.", "Worksheets.", "Tests.", "Courses."]}
+                  typeSpeed={150}
+                  backSpeed={50}
+                  loop
+              />
+            </span>
                     </h2>
                 </div>
 
@@ -60,21 +53,21 @@ const Hero: React.FunctionComponent = () => {
                 <div className="my-6 flex flex-col w-full lg:w-100 justify-center content-center">
                     <img className="m-auto w-96 lg:w-full" src={HeroIllustration}/>
                     <div className={"text-center text-2xl p-4 font-cursive mt-6"}>
-                        “{quotes[Math.floor(Math.random() * quotes.length)%quotes.length]}”
+                        “{quotes[Math.floor(Math.random() * quotes.length) % quotes.length]}
+                        ”
                     </div>
                 </div>
             </div>
 
             {/* Icon */}
             <div className={"text-center text-secondary p-2 mt-10 animate-bounce"}>
-                <FontAwesomeIcon className={"text-3xl lg:text-5xl"} icon={faAnglesUp} />
+                <FontAwesomeIcon className={"text-3xl lg:text-5xl"} icon={faAnglesUp}/>
             </div>
 
             {/* Wave SVGs */}
             <Wave/>
-
         </div>
-    )
-}
+    );
+};
 
 export default Hero;
