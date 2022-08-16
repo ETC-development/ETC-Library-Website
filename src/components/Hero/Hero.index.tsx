564b79f3a737411bc706e5ef407d963c4fb7395b
salahmak <salah.mak@protonmail.com>
2022-08-16 19:21:38 +0100

    Extracted Wave as a utility component

--- src/components/Hero/Hero.index.tsx
+++ src/components/Hero/Hero.index.tsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faAnglesUp} from "@fortawesome/free-solid-svg-icons";
 
 import quotes from "utils/quotes";
+import Wave from "../../utils/Wave";
 
 
 const Hero: React.FunctionComponent = () => {
@@ -64,37 +65,13 @@ const Hero: React.FunctionComponent = () => {
                 </div>
             </div>
 
-            {/* Motivational quote */}
-
             {/* Icon */}
             <div className={"text-center text-secondary p-2 mt-10 animate-bounce"}>
                 <FontAwesomeIcon className={"text-3xl lg:text-5xl"} icon={faAnglesUp} />
             </div>
 
             {/* Wave SVGs */}
-            <div className={"relative"}>
-                <div className="absolute w-full overflow-hidden leading-[0px]">
-                    <svg className="relative block w-[400%] h-[75px] origin-center" data-name="Layer 1"
-                         xmlns="http://www.w3.org/2000/svg"
-                         viewBox="0 0 1200 100"
-                         preserveAspectRatio="none">
-                        <path
-                            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                            className="fill-secondary"></path>
-                    </svg>
-                </div>
-
-                <div className="absolute w-full overflow-hidden leading-[0px]">
-                    <svg className="relative block w-[300%] h-[60px] origin-center" data-name="Layer 2"
-                         xmlns="http://www.w3.org/2000/svg"
-                         viewBox="0 0 1200 100"
-                         preserveAspectRatio="none">
-                        <path
-                            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                            className="fill-dark-bg"></path>
-                    </svg>
-                </div>
-            </div>
+            <Wave/>
 
         </div>
     )
