bb7719fe35b348b8d1da98c4c0fc6a4659f9c705
salahmak <salah.mak@protonmail.com>
2022-08-16 19:14:39 +0100

    added animated scroll icon and motivational quotes in Hero

--- src/components/Hero/Hero.index.tsx
+++ src/components/Hero/Hero.index.tsx
@@ -1,16 +1,20 @@
 import HeroIllustration from "assets/hero.svg"
 import Typed from "react-typed"
 import './Hero.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faAnglesUp} from "@fortawesome/free-solid-svg-icons";
+
+import quotes from "utils/quotes";
+
 
 const Hero: React.FunctionComponent = () => {
     return (
         <div className="flex flex-col bg-dark-bg text-light-text font-sans">
-
             {/* Illustration + Paragraph */}
             <div
-                className="bg-dark-bg lg:h-screen w-full flex-grow-0 flex-shrink flex flex-no-wrap flex-col-reverse justify-center content-center p-2 lg:p-10 lg:flex-row lg:flex-row lg:justify-around">
+                className="bg-dark-bg lg:h-fit w-full flex-grow-0 flex-shrink flex flex-no-wrap flex-col-reverse justify-center content-center px-2 lg:flex-row lg:flex-row lg:justify-around lg:px-10">
                 <div
-                    className="flex flex-shrink flex-col my-6 text-center lg:text-left gap-4 justify-center content-center">
+                    className="flex flex-shrink flex-col text-center lg:text-left gap-4 justify-center content-center">
                     <h1 className="hero-h1">
                         <span>Welcome to </span>
                         <span className="text-secondary">ENSIA Bib</span>
@@ -51,18 +55,28 @@ const Hero: React.FunctionComponent = () => {
                     </h2>
                 </div>
 
-                <div className="my-6 flex justify-center content-center">
+                {/* Illustration */}
+                <div className="my-6 flex flex-col w-full lg:w-100 justify-center content-center">
                     <img className="m-auto w-96 lg:w-full" src={HeroIllustration}/>
+                    <div className={"text-center text-2xl p-4 font-cursive mt-6"}>
+                        “{quotes[Math.floor(Math.random() * quotes.length)%quotes.length]}”
+                    </div>
                 </div>
             </div>
 
+            {/* Motivational quote */}
+
+            {/* Icon */}
+            <div className={"text-center text-secondary p-2 mt-10 animate-bounce"}>
+                <FontAwesomeIcon className={"text-3xl lg:text-5xl"} icon={faAnglesUp} />
+            </div>
 
             {/* Wave SVGs */}
             <div className={"relative"}>
                 <div className="absolute w-full overflow-hidden leading-[0px]">
-                    <svg className="relative block w-[400%] h-[75px] origin-center lg:w-[400%]" data-name="Layer 1"
+                    <svg className="relative block w-[400%] h-[75px] origin-center" data-name="Layer 1"
                          xmlns="http://www.w3.org/2000/svg"
-                         viewBox="0 0 1200 120"
+                         viewBox="0 0 1200 100"
                          preserveAspectRatio="none">
                         <path
                             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
@@ -71,9 +85,9 @@ const Hero: React.FunctionComponent = () => {
                 </div>
 
                 <div className="absolute w-full overflow-hidden leading-[0px]">
-                    <svg className="relative block w-[300%] h-[60px] origin-center lg:w-[300%]" data-name="Layer 2"
+                    <svg className="relative block w-[300%] h-[60px] origin-center" data-name="Layer 2"
                          xmlns="http://www.w3.org/2000/svg"
-                         viewBox="0 0 1200 120"
+                         viewBox="0 0 1200 100"
                          preserveAspectRatio="none">
                         <path
                             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
