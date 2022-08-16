ffe8f9982ddd0c7c706387848f25808c09a73853
Mohcen <hugoboy2311@gmail.com>
2022-08-17 00:43:39 +0200

    Added Font awesome icons

--- src/components/Footer/Footer.index.tsx
+++ src/components/Footer/Footer.index.tsx
@@ -1,18 +1,21 @@
 import "./Footer.css"
 
-// importing maiN pictures of the footer
+
+// importing main pictures of the footer
 import Computer from "../../assets/computer.svg"
 import math from "../../assets/math.svg"
 import Logo from './utils/ETC Logo.png'
 
 // importing the icons
-import  github  from './utils/github.png';
-import heart from "./utils/heart.png"
-import facebook from './utils/facebook.png'
-import instagram from './utils/instagram.png'
-import email from './utils/email.png'
-import linkedin from './utils/linkedin.png'
-import document from './utils/file-document.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faFileLines } from '@fortawesome/free-solid-svg-icons'
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faFacebook} from "@fortawesome/free-brands-svg-icons";
+import {faInstagram} from "@fortawesome/free-brands-svg-icons";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
 // importing the wave background
 import Wave from "./utils/Wave";
@@ -27,32 +30,34 @@ const Footer: React.FunctionComponent = () => {
                 <div>
                     <img src={Computer} alt="computer image" className="hidden md:inline"/>
                     <a href="https://github.com/ETC-development/ETC-Library-Website" className="w-[250px] md:w-[220px]  flex justify-center content-center px-2 py-1 mt-2 border-2 text-light-text rounded-xl border-violet">
-                        <img src={github} alt="github" className="h-7 w-7 violet-text mr-6 md:mr-3"/>
+                        <FontAwesomeIcon icon={faGithub as IconProp} className="text-2xl mr-3" />
                         Contribute on Github 
                     </a>
                 </div>
                 <div className="mt-10 text-center md:mt-[170px]">
-                    <h1 className="flex text-light-text text-2xl justify-center tracking-widest">Made with <img src={heart} alt="love" className="mx-2 w-8"/> by</h1>
+                    <h1 className="flex text-light-text text-2xl justify-center tracking-widest">Made with 
+                        <FontAwesomeIcon className="text-secondary mx-2 text-3xl"  icon={faHeart as IconProp} /> by
+                    </h1>
                     <img src={Logo} alt="ETC logo" className="h-60 w-54" />
                     <div className="flex justify-center">
                         <a href="https://web.facebook.com/ensia.tech.community/">
-                            <img src={facebook} alt="facebook" className="h-7 w-7"/>
+                            <FontAwesomeIcon icon={faFacebook as IconProp}  className="text-3xl text-violet mr-2"/>
                         </a>
                         <a href="https://www.instagram.com/etc_.club/">
-                            <img src={instagram} alt="instagram" className="h-7 w-7"/>
+                            <FontAwesomeIcon icon={faInstagram as IconProp} className="text-3xl text-violet mr-2"/>
                         </a>
                         <a href="https://www.linkedin.com/company/ensia-tech-community/?originalSubdomain=dz">
-                            <img src={linkedin} alt="linked in" className="h-7 w-7"/>
+                            <FontAwesomeIcon icon={faLinkedin as IconProp} className="text-3xl text-violet mr-2" />
                         </a>
                         <a href="https://www.google.com/intl/fr/gmail/about" className="last">
-                            <img src={email} alt="email" className="h-7 w-7"/>
+                            <FontAwesomeIcon icon={faEnvelope as IconProp} className="text-3xl text-violet "/>
                         </a>
                     </div>
                 </div>
                 <div>
                     <img src={math} alt="computer image" className="hidden md:inline md:mb-[20px]"/>
                     <a href="TODO" className="w-[250px] md:w-[220px] md:mb-[13px] flex justify-center content-center px-2 py-1 mt-10 md:mt-[0px] border-2 text-light-text rounded-xl border-violet">
-                        <img src={document} alt="github" className="h-7 w-7 violet-text mr-8 md:mr-5"/>
+                        <FontAwesomeIcon icon={faFileLines as IconProp} className="text-2xl mr-3" />
                         <p>Submit documents</p>
                     </a>
                 </div>
