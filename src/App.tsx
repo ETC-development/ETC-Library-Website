33522c8433a499df77f1eb712f233d4112e5fe0a
salahmak <salah.mak@protonmail.com>
2022-08-17 20:46:28 +0100

    Put Hero, Main and Footer together

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Hero from "./components/Hero/Hero.index";
 import Main from "./components/Main/Main.index";
+import Footer from "./components/Footer/Footer.index";
 
 interface AppProps {
 }
 
 const App: React.FunctionComponent<AppProps> = () => {
-    return (
-        <div>
-            <Hero/>
-            <Main/>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Hero />
+      <Main />
+      <Footer/>
+    </div>
+  );
+};
 
 export default App;
