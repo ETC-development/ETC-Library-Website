a2df5bc20f17067abd3bb72c4c9e49bb92374995
salahmak <salah.mak@protonmail.com>
2022-08-17 16:36:26 +0100

    add boilerplates for hero and main

---
+++ src/components/Hero/Hero.index.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Hero;
---
+++ src/components/Main/Main.index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Main;
