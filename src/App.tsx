34e44dbd4e64abb74b56f651d2c10361cdc730f3
salahmak <salah.mak@protonmail.com>
2022-08-17 20:35:59 +0100

    Integrated Soumia's Cards design in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import Hero from "./components/Hero/Hero.index";
+import Main from "./components/Main/Main.index";
 
 interface AppProps {
-  
 }
- 
+
 const App: React.FunctionComponent<AppProps> = () => {
-  return ( 
-    <div>
-      <Hero/>
-    </div>
-   );
+    return (
+        <div>
+            <Hero/>
+            <Main/>
+        </div>
+    );
 }
- 
+
 export default App;
---
+++ src/utils/modules.ts
@@ -0,0 +1,136 @@
+import { Year } from "interfaces/interfaces.index";
+
+const cardsInfo: Year[] = [
+    {
+        title: "First Preparatory Cycle",
+        semesters: [
+            {
+                title: "Semester 1",
+                modules: [
+                    {
+                        name: "Intensive English",
+                        url: "",
+                    },
+                    {
+                        name: "Critical Thinking and Creative Problem Solving (CPSCT)",
+                        url: "",
+                    },
+                    {
+                        name: "Analysis 1",
+                        url: "",
+                    },
+                    {
+                        name: "Algebra 1",
+                        url: "",
+                    },
+                    {
+                        name: "Academic Communication 1",
+                        url: "",
+                    },
+                ],
+            },
+            {
+                title: "Semester 2",
+                modules: [
+                    {
+                        name: "Analysis 2",
+                        url: "",
+                    },
+                    {
+                        name: "Introduction to Programming in C++",
+                        url: "",
+                    },
+                    {
+                        name: "Academic Communication 2",
+                        url: "",
+                    },
+                    {
+                        name: "Linear Algebra",
+                        url: "",
+                    },
+                    {
+                        name: "Probabilties and Statistics",
+                        url: "",
+                    },
+                    {
+                        name: "Object Oriented Programming",
+                        url: "",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        title: "Second Preparatory Cycle",
+        semesters: [
+            {
+                title: "Semester 1",
+                modules: [
+                    {
+                        name: "Machine Structure.",
+                        url: "",
+                    },
+                    {
+                        name: "Databases.",
+                        url: "",
+                    },
+                    {
+                        name: "Computer Architecture.",
+                        url: "",
+                    },
+                    {
+                        name: "Software Engineering.",
+                        url: "",
+                    },
+                    {
+                        name: "Data Structures.",
+                        url: "",
+                    },
+                    {
+                        name: "Introduction to AI.",
+                        url: "",
+                    },
+                    {
+                        name: "Introduction to Business.",
+                        url: "",
+                    },
+                ],
+            },
+            {
+                title: "Semester 2",
+                modules: [
+                    {
+                        name: "Machine Structure.",
+                        url: "",
+                    },
+                    {
+                        name: "Databases.",
+                        url: "",
+                    },
+                    {
+                        name: "Computer Architecture.",
+                        url: "",
+                    },
+                    {
+                        name: "Software Engineering.",
+                        url: "",
+                    },
+                    {
+                        name: "Data Structures.",
+                        url: "",
+                    },
+                    {
+                        name: "Introduction to AI.",
+                        url: "",
+                    },
+                    {
+                        name: "Introduction to Business.",
+                        url: "",
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+export default cardsInfo;
